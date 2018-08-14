2da8c107bc7c32c21f8a5d2b6376d11e406a1802
Nils Uliczka <nils.uliczka@darookee.net>
2018-08-14 21:52:16 +0200

    Use Grav-Escape instead of ESC

--- keyboards/planck/keymaps/maxplanck/keymap.c
+++ keyboards/planck/keymaps/maxplanck/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTZ] = LAYOUT_planck_grid(
-        KC_ESC,        KC_Q,    KC_W,    KC_E, KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,            KC_O,    KC_P,    DE_UE,
+        KC_GESC,       KC_Q,    KC_W,    KC_E, KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,            KC_O,    KC_P,    DE_UE,
         KC_TAB,        KC_A,    KC_S,    KC_D, KC_F,  KC_G,   KC_H,   KC_J,  KC_K,            KC_L,    DE_OE,   DE_AE,
         LSFT_T(DE_SS), KC_Z,    KC_X,    KC_C, KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM,         KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
         KC_LCTL,       KC_LGUI, KC_LALT, FUN,  LOWER, KC_SPC, KC_SPC, RAISE, RALT_T(KC_LEFT), KC_DOWN, KC_UP,   RCTL_T(KC_RGHT)
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-        DE_CIRC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_DEL,  DE_PIPE, DE_ACUT, DE_LCBR, DE_LBRC, DE_LPRN, DE_RPRN, DE_RBRC, DE_RCBR, DE_QUOT, DE_DQOT, DE_GRV,
         KC_LSFT, DE_BSLS, KC_HASH, DE_LESS, DE_COLN, DE_UNDS, DE_MINS, DE_SCLN, DE_MORE, DE_TILD, DE_SLSH, RSFT_T(KC_ENT),
         KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, KC_RALT, XXXXXXX, XXXXXXX, KC_RCTL
