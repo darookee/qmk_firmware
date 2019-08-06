52baeb6b5f34298a5dcfe50518448acdeb6ac275
Nils Uliczka <nils.uliczka@darookee.net>
2019-08-06 17:31:36 +0200

    Make firmware smaller

--- keyboards/planck/keymaps/maxplanck/config.h
+++ keyboards/planck/keymaps/maxplanck/config.h
@@ -2,6 +2,9 @@
 #define CONFIG_USER_H
 #define AUDIO_CLICKY
 
+#define NO_DEBUG
+#define NO_PRINT
+
 #define TAPPING_FORCE_HOLD
 
 #include "config_common.h"
@@ -31,7 +34,6 @@
    - MIDI notes can be sent when in Music mode is on
 */
 
-#define MIDI_BASIC
 
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
